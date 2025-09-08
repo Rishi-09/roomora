b10950384c9e78db992acce29915e8db6a30512a
Rishi <rishikm215@gmail.com>
2025-09-08 20:59:04 +0530

    created initialising dataset files

---
+++ init/index.js
@@ -0,0 +1,21 @@
+ const mongoose = require("mongoose");
+ const initData = require("./data");
+ const Listing = require("../models/listing");
+const { init } = require("../models/listing");
+
+
+ main()
+ .then(res=>console.log("connection successfull"))
+ .catch(err=>console.log(err));
+ 
+ async function main() {
+     await mongoose.connect("mongodb://127.0.0.1:27017/roomora");
+ }
+ 
+ const initDB = async () =>{
+    await Listing.deleteMany({});
+    await Listing.insertMany(initData.data);
+    console.log("data was initialised")
+ }
+
+ initDB(); 
