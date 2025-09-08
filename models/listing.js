581db9cd5cff78fd2fa2786d60ca7e68e01a4c59
Rishi <rishikm215@gmail.com>
2025-09-08 20:59:39 +0530

    updated listings model

--- models/listing.js
+++ models/listing.js
@@ -1,1 +1,23 @@
-const
+const express = require("express");
+const { default: mongoose } = require("mongoose");
+
+
+const listingSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:String,
+    image:{
+        type:String,
+        default:"https://images.unsplash.com/photo-1756926650683-27b0863be328?q=80&w=1175&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        set:(v)=> v==="" ? "https://images.unsplash.com/photo-1756926650683-27b0863be328?q=80&w=1175&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" :v,
+    },
+    price:Number,
+    location:String,
+    country:String
+})
+
+const Listing = mongoose.model("Listing",listingSchema);
+
+module.exports = Listing;
