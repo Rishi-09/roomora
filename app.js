faa533cfbe104bece4d567d8b51f1079f1022216
Rishi <rishikm215@gmail.com>
2025-10-06 07:34:26 +0530

    used passport lib for user authentication

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
+
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 main()
 .then(res=>console.log("connection successfull"))
@@ -41,16 +46,39 @@ const sessionOptions = {
 app.use(session(sessionOptions));
 app.use((flash()));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.use((req,res,next)=>{
     res.locals.success =req.flash("success");
     res.locals.error =req.flash("error");
+    res.locals.currUser = req.user;
     next();
 })
 
-app.use("/listings",listings);
-app.use("/listings/:id/reviews",reviews);
 
+
+app.get('/demouser',async (req,res)=>{
+    let fakeUser = new User({
+        email:"me@gmail.com",
+        username:"mee"
+    });
+
+    let regusteredUser = await User.register(fakeUser,"helloworld");
+    res.send(regusteredUser);
+})
+
+
+
+app.use("/listings",listingRouter);
+app.use("/listings/:id/reviews",reviewRouter);
+app.use("/",userRouter);
 
 app.use((req,res,next)=>{
     next(new ExpressError(404,"Page Not Found!"));
